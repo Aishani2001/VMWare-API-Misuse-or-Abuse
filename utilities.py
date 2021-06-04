9a53985b37877cd5f9d4e4f3e24bae2d82b37cb5
vrrao <vigneshravirao@gmail.com>
2021-06-05 03:44:57 +0530

    Feature engineering

--- utilities.py
+++ utilities.py
@@ -5,20 +5,22 @@ def extract_data(log_file):
     data = []
     log_file = open(log_file,'r')
     for log_line in log_file:
-        log_line = list(re.match(regex,log_line).groups())
-        parsed_line = {}
-        parsed_line['IP'] = log_line[0]
-        parsed_line['Timestamp'] = log_line[1]
-        parsed_line['URL'] = log_line[2]
-        parsed_line['Status Code'] = int(log_line[3])
-        if '-' in log_line[4]:
-            log_line[4] = 0
-        else:
-            log_line[4] = int(log_line[4])
-        parsed_line['Return Size'] = log_line[4]
-        parsed_line['Referer'] = log_line[5]
-        parsed_line['User Agent'] = log_line[6]
-        data.append(parsed_line)
+        log_line = re.match(regex,log_line)
+        if log_line:
+            log_line = list(log_line.groups())
+            parsed_line = {}
+            parsed_line['IP'] = log_line[0]
+            parsed_line['Timestamp'] = log_line[1]
+            parsed_line['URL'] = log_line[2]
+            parsed_line['Status Code'] = int(log_line[3])
+            if '-' in log_line[4]:
+                log_line[4] = 0
+            else:
+                log_line[4] = int(log_line[4])
+            parsed_line['Return Size'] = log_line[4]
+            parsed_line['Referer'] = log_line[5]
+            parsed_line['User Agent'] = log_line[6]
+            data.append(parsed_line)
     return data
 
 
